3bd58314d952667c4bcd8af25bdbaa878dc36c67
garyleejohnsonjr <garyjohnson@my.unt.edu>
2015-10-02 14:37:39 -0500

    setup parse

--- app/src/main/java/com/github/CSCE4444ElectronicRestrauntSystem/MainActivity.java
+++ app/src/main/java/com/github/CSCE4444ElectronicRestrauntSystem/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 
     @Override
