5808a1ba9df46388064d6081fda604b07f4ee7e4
desiherpin <desiherpin@verizon.net>
2015-10-09 03:50:33 -0500

    Parse Login
    You can create new users and go to there main pages. As of now there is
    a customer, server, and kitchen page.

---
+++ app/src/main/java/com/github/CSCE4444ElectronicRestrauntSystem/CreateUser.java
@@ -0,0 +1,111 @@
+package com.github.CSCE4444ElectronicRestrauntSystem;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import com.parse.ParseObject;
+
+//User Creation Page
+public class CreateUser extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+        /*Todo: Making note of basic syntax to create Parse Object
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+        Example: ParseObject name = new ParseObject("Name");
+                 name.put("Field", "Content");
+                 name.put("Field2", "Content");
+                 name.put("Calories", 320);
+                 name.saveInBackground();
+
+        Can also save associated user:
+                 ParseUser user = ParseUser.getCurrentUser();
+                 name.put("user", user);
+        */
+
+        //Create variables for buttons and edit text fields
+
+        Button bCreateUserInfo= (Button) findViewById(R.id.bCreateUserInfo);
+        final EditText etUserName = (EditText) findViewById(R.id.etUserName);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        bCreateUserInfo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Todo: Finish logic to confirm login info; current logic only temporary
+
+                //Grab user entered username and password and store as strings
+                String uName = etUserName.getText().toString();
+                String pswd = etPassword.getText().toString();
+                
+                //Gets Checkbox Info
+                String userType = "";
+                final CheckBox customerBox = (CheckBox) findViewById(R.id.customerBox);
+                final CheckBox kitchenBox = (CheckBox) findViewById(R.id.kitchenBox);
+                final CheckBox serverBox = (CheckBox) findViewById(R.id.serverBox);
+
+                //Gets type of user
+                if (customerBox.isChecked())
+                    userType = "Customer";
+                else if (kitchenBox.isChecked())
+                    userType = "Kitchen";
+                else if (serverBox.isChecked())
+                    userType = "Server";
+                //Todo: Need to add an else condition if multiple/no boxes are checked
+
+                //Creates New User
+                //Todo: General Error handling
+                ParseObject user = new ParseObject("Users");
+                user.put("Name", uName);
+                user.put("Password", pswd);
+                user.put("Job", userType);
+                user.saveInBackground();
+
+                //Todo: Sends user to main page for testing. Needs to go to individual homepage
+                Intent i = new Intent(CreateUser.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_create_user, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
--- app/src/main/java/com/github/CSCE4444ElectronicRestrauntSystem/MainActivity.java
+++ app/src/main/java/com/github/CSCE4444ElectronicRestrauntSystem/MainActivity.java
@@ -3,13 +3,19 @@ package com.github.CSCE4444ElectronicRestrauntSystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
         */
 
         //Create variables for buttons and edit text fields
-        Button bSubmitLoginInfo = (Button) findViewById(R.id.bSubmitLoginInfo);
+        Button bSubmitLoginInfo = (Button) findViewById(R.id.bCreateUserInfo);
+        Button bCreateNewUser = (Button) findViewById(R.id.bCreateNewUser);
         final EditText etUserName = (EditText) findViewById(R.id.etUserName);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
 
@@ -45,24 +52,98 @@ public class MainActivity extends AppCompatActivity {
                 //Todo: Finish logic to confirm login info; current logic only temporary
 
                 //Grab user entered username and password and store as strings
-                String uName = etUserName.getText().toString();
-                String pswd = etPassword.getText().toString();
+                final String uName = etUserName.getText().toString();
+                final String pswd = etPassword.getText().toString();
 
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Users");
+
+                query.whereEqualTo("Name", uName);
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> users,
+                                     ParseException e) {
+                        if (e == null)
+                        {
+                            for (ParseObject username: users) {
+                                //Log to check queries
+                                Log.d("Password", username.getString("Password"));
+                                Log.d("Job", username.getString("Job"));
+
+                                if(username.getString("Password").equals(pswd))
+                                {
+                                    //Sends to Customer Page
+                                    if (username.getString("Job").equals("Customer")) {
+                                        Intent i = new Intent(MainActivity.this, CustomerMain.class);
+                                        //example: i.putExtra("user", ParseUser.getCurrentUser().getString("name"));
+                                        startActivity(i);
+
+                                        finish();
+                                    }
+                                    //Sends to Kitchen Page
+                                    else if (username.getString("Job").equals("Kitchen")) {
+                                        Intent i = new Intent(MainActivity.this, KitchenMain.class);
+                                        //example: i.putExtra("user", ParseUser.getCurrentUser().getString("name"));
+                                        startActivity(i);
+
+                                        finish();
+                                    }
+                                    //Sends to Server Page
+                                    else if (username.getString("Job").equals("Server")) {
+                                        Intent i = new Intent(MainActivity.this, ServerMain.class);
+                                        //example: i.putExtra("user", ParseUser.getCurrentUser().getString("name"));
+                                        startActivity(i);
+
+                                        finish();
+                                    }
+                                    else if(uName.equals("manager") && pswd.equals("manager"))
+                                    {
+                                        //Todo: Manager login
+                                    }
+
+                                }
+
+                                //Incorrect Password Resets Page
+                                //Todo:Incorrect Password Message
+                                else
+                                {
+                                    finish();
+                                    startActivity(getIntent());
+                                }
+                            }
+                        }
+
+                        //Username Not Found
+                        //Todo:Incorrect Username Message
+                        else if(users.size() == 0)
+                        {
+                            finish();
+                            startActivity(getIntent());
+                        }
+                        else
+                        {
+                            //Failed Query Log
+                            Log.d("Name", "Error: " + e.getMessage());
+                        }
+
+                    }
+                });
                 //Verify login credentials
                 //Customer
-                if(uName.equals("customer") && pswd.equals("customer"))
-                {
-                    Intent i = new Intent(MainActivity.this, CustomerMain.class);
-                    //example: i.putExtra("user", ParseUser.getCurrentUser().getString("name"));
-                    startActivity(i);
 
-                    finish();
-                }
 
-                else if(uName.equals("manager") && pswd.equals("manager"))
-                {
-                    //Todo: Manager login
-                }
+            }
+        });
+
+        //Goes to Create User Page.
+        // Todo: Temporarily placed here, but the manager should be the only one to create new users
+
+        bCreateNewUser.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CreateUser.class);
+                startActivity(i);
+
+                finish();
             }
         });
     }
