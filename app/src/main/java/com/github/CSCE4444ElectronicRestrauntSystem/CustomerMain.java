22f1fc8d654ad24aaac126a440e88ab388ea4409
Whavron <whavron@gmail.com>
2015-11-09 19:45:39 -0600

    Added Customers Served Counter
    Displays under restaurant name in matching style

--- app/src/main/java/com/github/CSCE4444ElectronicRestrauntSystem/CustomerMain.java
+++ app/src/main/java/com/github/CSCE4444ElectronicRestrauntSystem/CustomerMain.java
@@ -5,6 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class CustomerMain extends AppCompatActivity {
 
@@ -12,6 +27,18 @@ public class CustomerMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main);
+        //setting Counter
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Order");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> counter, ParseException e) {
+                int count = counter.size();
+                TextView tvServedCounter= (TextView) findViewById(R.id.tvServedCounter);
+                String Formatted=String.valueOf(count)+ " Customers Served!";
+                tvServedCounter.setText(Formatted);
+            }
+        });
+
 
         Button bPayBill = (Button) findViewById(R.id.bPayBill);
         Button bViewMenu = (Button) findViewById(R.id.bCustomerViewMenu);
